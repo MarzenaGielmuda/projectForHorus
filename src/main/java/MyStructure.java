df1c1a2b7efcd2dbf053bac7b633208853f98401
Marzena Gielmuda <m.gielmuda@wp.pl>
2020-02-21 22:09:06 +0100

    override methods in MyStructure

--- src/main/java/MyStructure.java
+++ src/main/java/MyStructure.java
@@ -24,23 +24,43 @@
 //  List<INode> getNodes();
 //}
 
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.TreeMap;
+import java.util.function.Predicate;
 
 public class MyStructure implements IMyStructure {
 
-    private List<INode> nodes;
+    private List<INode> nodes = new LinkedList<>();
 
     public INode findByCode(String code) {
-        return null;
+        if( code != null){
+           return checkOption(x->code.equals(x.getCode()));
+        }else{
+            throw new IllegalArgumentException("Code is null");
+        }
+
     }
 
+
     public INode findByRenderer(String renderer) {
-        return null;
+        if (renderer != null){
+            return checkOption(x->renderer.equals(x.getRenderer()));
+        }else{
+            throw new IllegalArgumentException("Renderer is null");
+        }
     }
 
     public int count() {
-        return 0;
+        int countI = (int) nodes.stream().flatMap(INode::iNodeToStream).count();
+        return countI;
     }
+
+    private INode checkOption(Predicate<INode> predicate){
+        return nodes.stream().flatMap(INode::iNodeToStream).filter(predicate).findFirst().orElse(null);
+    }
+
 }
 
 
