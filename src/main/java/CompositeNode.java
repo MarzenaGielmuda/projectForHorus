2d9f04c54433543ffafb7ffd23916f92936c4009
Marzena Gielmuda <m.gielmuda@wp.pl>
2020-02-24 15:14:32 +0100

    add pom.xml to repository

--- src/main/java/CompositeNode.java
+++ src/main/java/CompositeNode.java
@@ -1,8 +1,11 @@
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class CompositeNode  extends Node implements ICompositeNode{
 
+    private List<INode> nodesList = new LinkedList<>();
 
     public CompositeNode(String code, String renderer) {
         super(code, renderer);
@@ -10,22 +13,31 @@ public class CompositeNode  extends Node implements ICompositeNode{
 
     @Override
     public List<INode> getNodes() {
-        return null;
-    }
-
-    @Override
-    public String getCode() {
-        return null;
-    }
-
-    @Override
-    public String getRenderer() {
-        return null;
+        return this.nodesList;
     }
 
     @Override
     public Stream<INode> iNodeToStream(){
-        return null;
+        return Stream.concat(super.iNodeToStream(),nodesList.stream().flatMap(INode::iNodeToStream));
 
+    }
+
+    public void addNode(INode iNode){
+        nodesList.add(iNode);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        CompositeNode that = (CompositeNode) o;
+        return Objects.equals(nodesList, that.nodesList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), nodesList);
     }
 }
